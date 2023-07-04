9760995e1b7bbf346209af79c94d70260a23d17f
KayKilb <115676011+KayKilb@users.noreply.github.com>
2023-07-04 11:05:07 -0500

    corrected bugged code

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
-# gets all states via python
+"""
+Write a script that lists all states from the database hbtn_0e_0_usa
+"""
 import MySQLdb
 import sys
 
-def main(args):
-    # gets all state stuff
-    if len(args) != 4:
-        raise Exception("need 3 arguments!")
-    db = MySQLdb.connect(host='localhost',
-                         user=args[1],
-                         passwd=args[2],
-                         db=args[3])
+
+def list_states():
+    """List the states"""
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    states = cur.fetchall()
-    for state in states:
-        print(state)
+
+    cur.execute("SELECT * FROM states ORDER BY id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    list_states()
