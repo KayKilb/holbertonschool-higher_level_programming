06fc5a99660f5498e402fca3cfb14a600a389a17
KayKilb <infulbloom@msn.com>
2023-08-01 14:03:25 -0700

    rectangle and mains execute

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -2,9 +2,9 @@
 // class Rectangle that defines a rectangle:
 
 class Rectangle {
-    constructor (w, h) {
-      this.width = w;
-      this.height = h;
-    }
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
   }
-  module.exports = Rectangle;
+}
+module.exports = Rectangle;
